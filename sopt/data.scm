a3b3bc5798978e5de74d0cd37d63dbe65d3cfcce
wasao <wasao@ac-room.org>
2015-12-04 10:18:16 +0900

    new parser

--- sopt/data.scm
+++ sopt/data.scm
@@ -4,7 +4,16 @@
   (export-all))
 (select-module sopt.data)
 
+;;;
+;;; sopt-args
+;;;
+
 (define-constant SOPT_UNDEF 'sopt--inner--undef)
+
+;; list as sopt-args
+(define-syntax make-sopt-args
+  (syntax-rules ()
+    [(_ arg ...) (list arg ...)]))
 
 (define (string->sopt-args str)
   (let ([lst (read-from-string str)])
@@ -12,158 +21,38 @@
       (error #"Invalid sopt-args from command-line: ~str"))
     (map (lambda (x) (if (eq? x '@undef) SOPT_UNDEF x)) lst)))
 
-;; sopt-cxt is a set of `sopt-fun`s
+;;;
+;;; sopt-cxt
+;;;
+
+;; sopt-cxt is a wrapper of <hash-table>, containing `sopt-def`s
+(define-record-type sopt-cxt %make-sopt-cxt #t ht)
+
 (define (port->sopt-cxt iport)
-  (port->list (compose sopt-parse read) iport))
+  (%make-sopt-cxt (hash-table 'eq? (port->list (compose sopt-parse read) iport))))
 
 (define (write-sopt-cxt cxt oport)
-  (for-each
-   (lambda (f)
-     (display (sopt-deparse f) oport)
-     (newline oport))
-   cxt))
+  (hash-table-for-each (sopt-cxt-ht cxt)
+    (lambda (name def)
+      (display (sopt-deparse def) oport)
+      (newline oport))))
 
 (define (sopt-cxt-ref cxt name)
-  (find (^[f] (eq? (fun-name f) name)) cxt))
+  (ref (sopt-cxt-ht cxt) name #f))
 
-(define (list->sopt-cxt lst)
-  lst)
-
-(define (sopt-parse s)
-  (define parse-pat
-    (match-lambda
-      [() '()]
-
-      [(ca . cd) (cons (make-var ca) (make-var cd))]
-      ))
-
-  (define parse-binding
-    (match-lambda
-     [(v t) (cons (sopt-parse v) (sopt-parse t))]))
-
-  (match s
-   [('define (name . args) . body)
-    (make-fun name (map sopt-parse args) (map sopt-parse body))]
-
-   [('if t1 t2 t3)
-    (make-ifs (sopt-parse t1)
-              (sopt-parse t2)
-              (sopt-parse t3))]
-
-   [('case t0 . clauses)
-    (make-cas (sopt-parse t0)
-              (map (^[c] (cons (parse-pat (car c)) (sopt-parse (cadr c))))
-                   clauses))]
-
-   [('quote lst) lst]
-
-   [('let bindings . body)
-    (make-les (map parse-binding bindings)
-              (map sopt-parse body))]
-
-   [(fun . args)
-    (make-app (car s) (map sopt-parse (cdr s)))]
-
-   [else
-    (if (symbol? s)
-        (make-var s)
-        s)]
-   ))
-
-(define (sopt-deparse p)
-  (define deparse-pat
-    (match-lambda
-      [() '()]
-
-      [(ca . cd) (cons (var-name ca) (var-name cd))]
-      ))
-
-  (define deparse-binding
-    (match-lambda
-     [(v . t) (list (sopt-deparse v) (sopt-deparse t))]))
-
-  (cond
-   [(fun? p)
-    `(define (,(fun-name p) . ,(map sopt-deparse (fun-args p)))
-       . ,(map sopt-deparse (fun-body p)))]
-
-   [(ifs? p)
-    `(if ,(sopt-deparse (ifs-test p))
-         ,(sopt-deparse (ifs-then p)) ,(sopt-deparse (ifs-else p)))]
-
-   [(cas? p)
-    `(case ,(sopt-deparse (cas-key p))
-       .
-       ,(map
-         (^[c]
-           (list (deparse-pat (car c)) (sopt-deparse (cdr c))))
-         (cas-clauses p)))]
-
-   [(les? p)
-    `(let ,(map deparse-binding (les-bindings p))
-       .  ,(map sopt-deparse (les-body p)))]
-
-   [(pair? p)
-    `(quote ,p)]
-
-   [(app? p)
-    `(,(app-fun-name p) . ,(map sopt-deparse (app-args p)))]
-
-   [(var? p)
-    (var-name p)]
-
-   [(symbol? p)
-    `(quote ,p)]
-
-   [else p]
-   ))
-
-
-;;
-;; q ::= d1 .. dM
-;;
-;; d ::= (define (f v1 .. vN) t1 .. tM)
-;;
-;; t ::= value
-;;     | v                                    [variable]
-;;     | (f t1 .. tN)                         [function call]
-;;     | (if t1 t2 t3)                        [conditional]
-;;     | (case t0 (p1 t1) ..)                 [case-expression]
-;;     | (let ((v1 t1) .. (vN tN)) t1 .. tM)  [let-expression]
-;;
-;; p ::= (ca . cd) | ()
-;;
-
-(define-record-type fun #t #t
-  name args body)
-
-(define-record-type app #t #t
-  fun-name args)
-
-(define-record-type cas #t #t
-  key clauses)
-
-(define-record-type ifs #t #t
-  test then else)
-
-(define-record-type les #t #t ; let
-  bindings body)
-
-(define-record-type var #t #t
-  name)
-
-(define-method object-equal? ((a var) (b var))
-  (eq? (var-name a) (var-name b)))
+;;;
+;;; syntax
+;;;
 
 ;;;;;;;;;;;;
 ;;
-;; program ::= def1 ...
+;; cxt     ::= hashtable((name . def) ...)
 ;;
-;; def     ::= (define var term)
+;; def     ::= (define (name . args) term)
 ;;
 ;; var     ::= symbol
 ;;
-;; literal ::= symbol | ...          [Gauche-type]
+;; literal ::= symbol | ...          // Gauche-types
 ;;
 ;; term    ::= var                   [var]
 ;;           | literal
@@ -171,34 +60,81 @@
 ;;           | (if test-term
 ;;                 then-term
 ;;                 else-term)        [if]
-;;           | (lambda formals term) [lambda]
-;;           | (set! var term)       [set!]
-;;           | (call/cc term)        [call/cc]
-;;           | (letrec
+;;           | (let
 ;;               ((var1 term1)
 ;;                ...
 ;;                (varN-1 termN-1))
-;;               termN)              [letrec]
-;;           | (apply term ...)      [apply]
+;;               . terms)            [let]
+;;           | (apply term lst)      [apply]
+;;           | (lambda args . terms) [lambda]
+;;           | (call/cc term)        [call/cc]
+;;           | (set! var term)       [set!]
 ;;           | (term1 ...)           [call]
 ;;
 
-;; (define-record-type sopt-var     #t #t name)
+(define (sopt-parse def)
+  (match def
+    [(or ('define (name . args) . terms)
+         ('define name ('lambda args . terms)))
+     (make-sopt-def name args (map sopt-parse-term terms))]))
 
-;; (define-record-type sopt-obj     #t #t value)
+(define-syntax (sopt-var? t)
+  (syntax-rules ()
+    [(_ t) (symbol? t)]))
 
-;; (define-record-type sopt-if      #t #t test     then else)
+(define (sopt-parse-term term)
+  (match term
+    [('quote literal)
+     (make-sopt-literal literal)]
 
-;; (define-record-type sopt-letrec  #t #t bindings body)
+    [('if test-term then-term else-term)
+     (make-sopt-if
+      (sopt-parse-term test-term)
+      (sopt-parse-term then-term)
+      (sopt-parse-term else-term))]
 
-;; (define-record-type sopt-lambda  #t #t formals  term)
+    [('let bindings . terms)
+     ;; use alist as bindings
+     (make-sopt-let
+      (map
+       (lambda (b)
+         (let ((b-var (car b)) (b-term (cadr b)))
+           (cons b-var (sopt-parse-term b-term))))
+       bindings)
+      (map sopt-parse-term terms))]
 
-;; (define-record-type sopt-set!    #t #t var      term)
+    [('apply t1 t2)
+     (make-sopt-apply (sopt-parse-term t1) (sopt-parse-term t2))]
 
-;; (define-record-type sopt-call/cc #t #t proc)
+    [(or ('call/cc t) ('call-with-current-continuation t))
+     (make-sopt-call/cc (sopt-parse-term t))]
 
-;; (define-record-type sopt-apply   #t #t proc     args)
+    [('lambda args . terms)
+     (make-sopt-lambda #f args (map sopt-parse-term terms))]
 
-;;
-;; * lambda as a object
-;;
+    [('set! var t)
+     (make-sopt-set! var (sopt-parse-term t))]
+
+    [(term-fun . args-term
+     (make-sopt-call
+      (sopt-parse-term term-fun)
+      (map sopt-parse-term args-term)))]
+
+    [else
+     (if (symbol? term) term (make-sopt-literal))]))
+
+(define-record-type sopt-def     #t #f name args terms)
+
+(define-record-type sopt-literal #t #t value)
+
+(define-record-type sopt-if      #t #t test then else)
+
+(define-record-type sopt-let     #t #t bindings terms)
+
+(define-record-type sopt-apply   #t #t proc lst)
+
+(define-record-type sopt-call/cc #t #t proc)
+
+(define-record-type sopt-lambda  #t #t frame args terms)
+
+(define-record-type sopt-set!    #t #t var term)
