877124d6979e1df76ec4b62e89a3f57499040ea5
wasao <wasao@ac-room.org>
2015-12-04 11:36:21 +0900

    Limit exporting of sopt-module

--- sopt.scm
+++ sopt.scm
@@ -1,1 +1,11 @@
-(define-module sopt (extend sopt.data sopt.eval))
+(define-module sopt
+  (use sopt.data)
+  (use sopt.eval)
+  (export
+   string->sopt-args
+   make-sopt-args
+   SOPT_UNDEF
+   sopt-eval
+   port->sopt-cxt
+   write-sopt-cxt
+   ))
